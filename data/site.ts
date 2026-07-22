dd34fdd63acd3239c1e5135d9c78ee5258760101
quockhanh41 <quockhanh4104.kn@gmail.com>
2026-07-23 01:25:09 +0700

    feat(contact): update exact Google Maps place URL for PCCC Bình An

--- data/site.ts
+++ data/site.ts
@@ -10,9 +10,12 @@ export const site = {
   facebook: "https://facebook.com/",
   email: "lienhe@pcccpro.example",
   address: "Thiết bị Phòng Cháy Chữa Cháy Bình An",
-  mapUrl: "https://maps.app.goo.gl/wAZM7yubLFZ3Y9xw6",
-  // Google Maps embed URL dựa trên vị trí chính xác:
-  mapEmbedUrl: "https://www.google.com/maps?q=15.2941708,108.7624401&z=16&output=embed",
+  // Đường dẫn mở trực tiếp Google Maps chính xác của cơ sở:
+  mapUrl:
+    "https://www.google.com/maps/place/Thi%E1%BA%BFt+b%E1%BB%8B+Ph%C3%B2ng+Ch%C3%A1y+Ch%E1%BB%AFa+Ch%E1%BB%AFa+B%C3%ACnh+An/@15.294176,108.7598652,17z/data=!4m16!1m9!3m8!1s0x3169b50036633fa7:0x784e4d80fc268f2f!2zVGhp4bq_dCBi4buLIFBow7JuZyBDaMOheSBDaOG7r2EgQ2jDoXkgQsOsbmggQW4!8m2!3d15.2941708!4d108.7624401!9m1!1b1!16s%2Fg%2F11nc4z7t3v!3m5!1s0x3169b50036633fa7:0x784e4d80fc268f2f!8m2!3d15.2941708!4d108.7624401!16s%2Fg%2F11nc4z7t3v",
+  // Embed iframe Google Maps theo place ID của địa điểm:
+  mapEmbedUrl:
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3848.5158548549143!2d108.7598652!3d15.294176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3169b50036633fa7%3A0x784e4d80fc268f2f!2zVGhp4bq_dCBi4buLIFBow7JuZyBDaMOheSBDaOG7r2EgQ2jDoXkgQsOsbmggQW4!5e0!3m2!1svi!2svn",
   // Formspree: tạo form miễn phí tại formspree.io, dán endpoint https://formspree.io/f/xxxx
   formspreeEndpoint: "https://formspree.io/f/your-id-here",
 } as const;
