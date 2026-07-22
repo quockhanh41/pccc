e3d3663311e545b0b354ba83548229faaade2315
quockhanh41 <quockhanh4104.kn@gmail.com>
2026-07-23 01:29:34 +0700

    fix(site): update phone number and Zalo link to 0789686363

--- data/site.ts
+++ data/site.ts
@@ -4,12 +4,12 @@ export const site = {
   description:
     "Cung cấp thiết bị phòng cháy chữa cháy đạt chuẩn, kiểm định đầy đủ: bình chữa cháy, thiết bị báo cháy, vòi & lăng chữa cháy, trang phục bảo hộ, đèn & biển báo thoát hiểm.",
   url: "https://pccc-pro.vercel.app",
-  phone: "0900 000 000",
-  phoneDigits: "0900000000",
-  zalo: "https://zalo.me/0900000000",
+  phone: "0789 686 363",
+  phoneDigits: "0789686363",
+  zalo: "https://zalo.me/0789686363",
   facebook: "https://facebook.com/",
   email: "lienhe@pcccpro.example",
-  address: "Thiết bị Phòng Cháy Chữa Cháy Bình An",
+  address: "Thiết bị Phòng Cháy Chữa Cháy Bình An, 505 Phạm Văn Đồng, Bình Sơn, Quảng Ngãi",
   // Đường dẫn mở trực tiếp Google Maps chính xác của cơ sở:
   mapUrl:
     "https://www.google.com/maps/place/Thi%E1%BA%BFt+b%E1%BB%8B+Ph%C3%B2ng+Ch%C3%A1y+Ch%E1%BB%AFa+Ch%E1%BB%AFa+B%C3%ACnh+An/@15.294176,108.7598652,17z/data=!4m16!1m9!3m8!1s0x3169b50036633fa7:0x784e4d80fc268f2f!2zVGhp4bq_dCBi4buLIFBow7JuZyBDaMOheSBDaOG7r2EgQ2jDoXkgQsOsbmggQW4!8m2!3d15.2941708!4d108.7624401!9m1!1b1!16s%2Fg%2F11nc4z7t3v!3m5!1s0x3169b50036633fa7:0x784e4d80fc268f2f!8m2!3d15.2941708!4d108.7624401!16s%2Fg%2F11nc4z7t3v",
